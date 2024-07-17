a04c1c9d34630a108bba65fe43cd9c787b2c8dab
Rich Fiorella <rfiorella@lanl.gov>
2024-07-16 20:37:12 -0600

    wip - im3 ens

--- runcase.py
+++ runcase.py
@@ -1902,7 +1902,7 @@ if ((options.ensemble_file != '' or int(options.mc_ensemble) != -1) and (options
             print('Error:  ensemble file does not exist')
             sys.exit(1)
 
-        samples=numpy.zeros((n_parameters,100000), dtype=numpy.float)
+        samples=numpy.zeros((n_parameters,100000), dtype=numpy.float64)
         #get parameter samples and information
         myinput=open(options.ensemble_file)
         nsamples = 0
@@ -1913,7 +1913,7 @@ if ((options.ensemble_file != '' or int(options.mc_ensemble) != -1) and (options
         myinput.close()
     elif (int(options.mc_ensemble) > 0):
         nsamples = int(options.mc_ensemble)
-        samples=numpy.zeros((n_parameters,nsamples), dtype=numpy.float)
+        samples=numpy.zeros((n_parameters,nsamples), dtype=numpy.float64)
         for i in range(0,nsamples):
             for j in range(0,n_parameters):
                 samples[j][i] = param_min[j]+(param_max[j]-param_min[j])*numpy.random.rand(1)
